141529e6658b007fe694e9238e4cf8d37bb59f30
rivalcoba <ivan.rivalcoba@gmail.com>
2023-09-21 06:01:22 -0500

    ⭐ Feature: API GPT API and documentation included

--- routes/api.js
+++ routes/api.js
@@ -16,9 +16,31 @@ const openai = new OpenAIApi(configuration)
 
 /* GET api */
 router.post("/", async function (req, res, next) {
+  const {
+    question01, 
+    question02, 
+    question03, 
+    question04, 
+    question05, 
+  } = req.body;
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: "Dame 3 lugares de Oaxaca para visitar" }],
+    messages: [{ role: "user", content: `
+    Estas en la ciudad de Oaxaca, y con base en la respuesta a estas preguntas:
+    1. ¿Cuantos años tienes?
+    Respuesta: ${question01}
+    2. ¿Quienes viajan contigo y que edad tiene cada uno de tus acompañantes?
+    Respuesta: ${question02}
+    3. ¿Que experiencias te gustaria probar?
+    Respuesta: ${question03}
+    4. ¿Cual es tu presupuesto?
+    Respuesta: ${question04}
+    5. ¿Que hora es?
+    Respuesta: ${question05}
+    Elabora una ruta personalizada para un paseo en oaxaca dando como resultado una lista de lugares. 
+    Asegurate de que la lista contemple la hora, 
+    de modo que el número de actividades sea alcanzable antes de las 20 horas.
+    ` }],
   });
   res.json(completion.data.choices[0].message.content);
 });
