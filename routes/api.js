e188a00f041dddf4c9636f2244a6528a705939c8
Jennifer Aguado <108686161+JenniferAw26@users.noreply.github.com>
2023-09-21 13:20:25 +0000

    iconos

--- routes/api.js
+++ routes/api.js
@@ -37,12 +37,27 @@ router.post("/", async function (req, res, next) {
     Respuesta: ${question04}
     5. ¿Que hora es?
     Respuesta: ${question05}
-    Elabora una ruta personalizada para un paseo en oaxaca dando como resultado una lista de lugares. 
+    Elabora una ruta personalizada de maximo 5 puntos a visitar para un paseo en oaxaca dando como resultado una lista de lugares. 
     Asegurate de que la lista contemple la hora, 
     de modo que el número de actividades sea alcanzable antes de las 20 horas.
+    La respuesta que espero es una lista numerada con un emojie asociado al lugar y el nombre del lugar, solo eso
+    A continuacion te presento un ejemplo de como me gustaria la ruta personalizada:
+    1. 📍 Hora y Nombre del primer lugar a visitar.
+    2. 🦗 Nombre del segundo lugar a visitar.
+    ...
+    5. ⭐ Nombre del segundo lugar a visitar.
     ` }],
   });
   res.json(completion.data.choices[0].message.content);
 });
   
 module.exports = router;
+
+
+/*
+Vinder: 1. 🏛️ 8:30 am - Museo de las Culturas de Oaxaca 
+2. 🌮 10:00 am - Mercado 20 de Noviembre para probar la comida local, como los tlayudas y el mole 
+3. 🏢 12:00 pm - Centro Cultural Santo Domingo para apreciar la arquitectura y exposiciones de arte 
+4. 🌳 2:00 pm - Jardín Etnobotánico de Oaxaca, un oasis de naturaleza en el centro de la ciudad 
+5. 🛍️ 4:00 pm - Calle Macedonio Alcalá para disfrutar de las tiendas de artesanías y souvenirs oaxaqueños
+*/
