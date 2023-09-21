7b6f48efad18fafab666d4d17efb5c360145a80e
Joshua Barajas <124693455+JoshBarajas21@users.noreply.github.com>
2023-09-21 06:21:03 +0000

    🚧Feature: Forms Rutes Adding forms' routes to routes and views paths

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'HackaTec - Inicio Sesion', style: "/stylesheets/style.css"});
 });
-
+  
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/api', function(_, res) {
+  res.json({
+    name: "vinder",
+    members: {
+      m1: "Uriel",
+      m2: "Jenny",
+      m3: "Josh"
+    }
+  });
 });
 
 module.exports = router;
